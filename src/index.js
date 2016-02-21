1ba79a86a609f393ae452eace43240299d37f67f
Wojciech Zawistowski <wojciech.zawistowski@gmail.com>
2016-02-21 06:36:48 +0000

    initial react structure (index.js + index.html)

---
+++ src/index.js
@@ -0,0 +1,9 @@
+require('file?name=index.html!./index.html');
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+ReactDOM.render(
+  <h1>Matchnimals</h1>,
+  document.getElementById('matchnimals-app')
+);

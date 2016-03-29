a57482e69e25c4bedf2c6c5dd1bc8657420b0406
Wojciech Zawistowski <wojciech.zawistowski@gmail.com>
2016-03-29 16:09:35 +0000

    added Babel polyfill

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 require('file?name=index.html!./index.html');
 
+import 'babel-polyfill';
+
 import React from 'react';
 import { render } from 'react-dom';
 import { GameBoard } from './components/game_board/game_board';
